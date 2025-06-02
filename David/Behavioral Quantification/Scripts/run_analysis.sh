c2a646f3b9ad8a6bdcb7f8872d4768f0990d8d92
Davimeleon <96394073+Davimeleon@users.noreply.github.com>
2025-06-02 13:13:09 -0400

    modify slurm file

--- David/Behavioral Quantification/Scripts/run_analysis.sh
+++ David/Behavioral Quantification/Scripts/run_analysis.sh
@@ -8,6 +8,7 @@
 # Load the Python module (make sure 3.8 exists — check with `module avail python`)
 module load Python/3.10.8-GCCcore-12.2.0
 module load matplotlib/3.7.0-gfbf-2022b
+module load h5py/3.8.0-foss-2022b
 
 # Run your script
 python3 graph_creator.py
