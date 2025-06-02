53c138cb03d2a4c35afcbf03a0832605b60705be
Davimeleon <96394073+Davimeleon@users.noreply.github.com>
2025-06-02 13:20:09 -0400

    modify slurm file

--- David/Behavioral Quantification/Scripts/run_analysis.sh
+++ David/Behavioral Quantification/Scripts/run_analysis.sh
@@ -5,10 +5,16 @@
 #SBATCH --time=01:00:00 # Adjust time here
 #SBATCH --cpus-per-task=2
 
+# Load conda
+module load Miniconda3
+
+# Activate your conda environment
+source activate myenv
+
 # Load the Python module (make sure 3.8 exists — check with `module avail python`)
-module load Python/3.10.8-GCCcore-12.2.0
-module load matplotlib/3.7.0-gfbf-2022b
-module load h5py/3.8.0-foss-2022b
+#module load Python/3.10.8-GCCcore-12.2.0
+#module load matplotlib/3.7.0-gfbf-2022b
+#module load h5py/3.8.0-foss-2022b
 
 # Run your script
 python3 graph_creator.py
