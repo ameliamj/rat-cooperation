7a9b1e1c886ca414ebe5031f4a32616f173e25fe
Davimeleon <96394073+Davimeleon@users.noreply.github.com>
2025-06-05 22:37:06 -0400

    test PT vs TC

--- David/Behavioral_Quantification/Scripts/graph_creator.py
+++ David/Behavioral_Quantification/Scripts/graph_creator.py
@@ -424,7 +424,7 @@ posFiles = [["/Users/david/Documents/Research/Saxena_Lab/rat-cooperation/David/B
 
 
 #Paired Testing vs. Training Cooperation
-'''
+
 print("Running Paired Testing vs Training Cooperation")
 dataPT = getOnlyPairedTesting()
 dataTC = getOnlyTrainingCoop()
@@ -432,18 +432,18 @@ dataTC = getOnlyTrainingCoop()
 levFiles = [dataPT[0], dataTC[0]]
 magFiles = [dataPT[1], dataTC[1]]
 posFiles = [dataPT[2], dataTC[2]]
-categoryExperiments = multiFileGraphsCategories(magFiles, levFiles, posFiles, ["Paired_Testing", "Training_Cooperation"])'''
+categoryExperiments = multiFileGraphsCategories(magFiles, levFiles, posFiles, ["Paired_Testing", "Training_Cooperation"])
 
 
 #Unfamiliar vs. Training Partners
-print("Running UF vs TP")
+'''print("Running UF vs TP")
 dataUF = getOnlyUnfamiliar() #Unfamiliar
 dataTP = getOnlyTrainingPartners() #Training Partners
 
 levFiles = [dataUF[0], dataTP[0]]
 magFiles = [dataUF[1], dataTP[1]]
 posFiles = [dataUF[2], dataTP[2]]
-categoryExperiments = multiFileGraphsCategories(magFiles, levFiles, posFiles, ["Unfamiliar", "Training Partners"])
+categoryExperiments = multiFileGraphsCategories(magFiles, levFiles, posFiles, ["Unfamiliar", "Training Partners"])'''
 
 
 #Transparent vs. Translucent vs. Opaque
