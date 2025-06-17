9d766ae4d0d41933feee2096f76af6d23dc4ecbb
Davimeleon <96394073+Davimeleon@users.noreply.github.com>
2025-06-17 15:45:33 -0400

    change translucent to transparent

--- David/Behavioral_Quantification/Scripts/graph_creator.py
+++ David/Behavioral_Quantification/Scripts/graph_creator.py
@@ -2149,7 +2149,7 @@ def getFiltered():
     fe = fileExtractor(all_valid)
     fe.getPairedTestingSessions(sortOut=False)
     fe.getTrainingPartner(sortOut=False)
-    fe.getTranslucentSessions(sortOut=False)
+    fe.getTransparent(sortOut=False)
     fpsList, totFramesList = fe.returnFPSandTotFrames()
     return [fe.getLevsDatapath(), fe.getMagsDatapath(), fe.getPosDatapath(), fpsList, totFramesList]
 
