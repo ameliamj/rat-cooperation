d4404ab8aa5faa7c15e0da6e6df8a16e73780aa6
Davimeleon <96394073+Davimeleon@users.noreply.github.com>
2025-06-17 14:35:19 -0400

    run transparency

--- David/Behavioral_Quantification/Scripts/graph_creator.py
+++ David/Behavioral_Quantification/Scripts/graph_creator.py
@@ -586,19 +586,19 @@ categoryExperiments = multiFileGraphsCategories(magFiles, levFiles, posFiles, ["
 
 #Unfamiliar vs. Training Partners
 print("Running UF vs TP")
-dataUF = getOnlyUnfamiliar() #Unfamiliar
+'''dataUF = getOnlyUnfamiliar() #Unfamiliar
 dataTP = getOnlyTrainingPartners() #Training Partners
 
 levFiles = [dataUF[0], dataTP[0]]
 magFiles = [dataUF[1], dataTP[1]]
 posFiles = [dataUF[2], dataTP[2]]
 categoryExperiments = multiFileGraphsCategories(magFiles, levFiles, posFiles, ["Unfamiliar", "Training Partners"])
-
+'''
 
 #Transparent vs. Translucent vs. Opaque
 
 #print("Running Transparency")
-'''dataTransparent = getOnlyTransparent() #Transparent
+dataTransparent = getOnlyTransparent() #Transparent
 dataTranslucent = getOnlyTranslucent() #Translucent
 dataOpaque = getOnlyOpaque() #Opaque
 
@@ -606,7 +606,7 @@ levFiles = [dataTransparent[0], dataTranslucent[0], dataOpaque[0]]
 magFiles = [dataTransparent[1], dataTranslucent[1], dataOpaque[1]]
 posFiles = [dataTransparent[2], dataTranslucent[2], dataOpaque[2]]
 categoryExperiments = multiFileGraphsCategories(magFiles, levFiles, posFiles, ["Transparent", "Translucent", "Opaque"])
-'''
+
 
 categoryExperiments.compareGazeEventsCategories()
 categoryExperiments.compareSuccesfulTrials()
