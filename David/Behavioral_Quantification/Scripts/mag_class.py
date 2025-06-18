e7bf114a1ebd00f45716e1b4980ec71b92671edb
Davimeleon <96394073+Davimeleon@users.noreply.github.com>
2025-06-17 22:05:06 -0400

    test

--- David/Behavioral_Quantification/Scripts/mag_class.py
+++ David/Behavioral_Quantification/Scripts/mag_class.py
@@ -20,7 +20,11 @@ class magLoader:
         self.filename = filename
         self.data = None
         self._load_data()
+        print("Data Before: ")
+        print(self.data)
         self._delete_bad_data()
+        print("Data after: ")
+        print(self.data)
         self.categories = ["TrialNum", "MagNum", "AbsTime", "Duration", "TrialCond", "DispTime", "TrialTime", "Hit", "TrialEnd", "RatID"]
         
     def _load_data(self): #Uses pandas to read csv file and store in a pandas datastructure
