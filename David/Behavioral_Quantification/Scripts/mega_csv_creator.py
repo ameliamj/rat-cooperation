c85a620a089d0cee5c97fbd0b788c56faa2b16eb
Davimeleon <96394073+Davimeleon@users.noreply.github.com>
2025-06-18 10:48:49 -0400

    Updated PT vs TC

---
+++ David/Behavioral_Quantification/Scripts/mega_csv_creator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 18 10:46:04 2025
+
+@author: david
+"""
+
+class megaCSVCreator:
+    def __init__(self, experiment):
+        self.exp = experiment
+        self._createUnifiedCSV
+    
+    def _createUnifiedCSV(self):
+        a = 1
