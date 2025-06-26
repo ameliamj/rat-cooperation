3536048f540d36d6da2df08a01c69b563eb8bc23
Davimeleon <96394073+Davimeleon@users.noreply.github.com>
2025-06-26 14:01:17 -0400

    run fiberPhoto

--- David/Behavioral_Quantification/Scripts/experiment_class.py
+++ David/Behavioral_Quantification/Scripts/experiment_class.py
@@ -19,9 +19,9 @@ class singleExperiment:
         self.lev_file = lev_file
         self.pos_file = pos_file
         
-        self.mag = magLoader(mag_file, fps=fps)
-        self.lev = levLoader(lev_file, endFrame, fps)
-        self.pos = posLoader(pos_file, endFrame)
+        #self.mag = magLoader(mag_file, fps=fps)
+        #self.lev = levLoader(lev_file, endFrame, fps)
+        #self.pos = posLoader(pos_file, endFrame)
         if (not fp_files is None and len(fp_files) == 3):
             self.fp = fiberPhotoLoader(fp_files[0], fp_files[1], fp_files[2])
         
