f3045fb6d5f82013a924d80eefe1593932130ff0
Davimeleon <96394073+Davimeleon@users.noreply.github.com>
2025-07-24 17:22:39 -0400

    test

--- David/Behavioral_Quantification/Scripts/graph_creator.py
+++ David/Behavioral_Quantification/Scripts/graph_creator.py
@@ -1791,7 +1791,7 @@ class MicePairGraphs:
         session_counts_EB = {}
     
         for group_idx, group in enumerate(self.experimentGroups):
-            if (len(group) < 5):
+            if (len(group) < 19):
                 continue
             for exp_idx, exp in enumerate(group):
                 lev = exp.lev
