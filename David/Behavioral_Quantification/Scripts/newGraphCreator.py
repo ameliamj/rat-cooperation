cb755c7feef745f2ed00dd03bdcabd17127d2e9a
DavidBackerPeral <david.backerperal@yale.edu>
2025-09-26 16:57:40 -0400

    test

--- David/Behavioral_Quantification/Scripts/newGraphCreator.py
+++ David/Behavioral_Quantification/Scripts/newGraphCreator.py
@@ -12,6 +12,8 @@ import numpy as np
 import pandas as pd
 import math
 import networkx as nx
+from datetime import datetime
+
 #import os
 
 from experiment_class import singleExperiment
@@ -102,7 +104,7 @@ class dataAnalysis:
         
         print(f"Deleted {deleted_count} experiment(s) due to missing categories.")
         
-    def generateLeftRightData(self, midline=696, bodypart=3):
+    def generateLeftRightHeatmapData(self, bodypart=3):
         """
         Generate concatenated left vs right position data across experiments.
         Returns: left_x, left_y, right_x, right_y
@@ -112,23 +114,45 @@ class dataAnalysis:
 
         for i, exp in enumerate(self.experiments):
             pos = exp.pos
-            for r in range(2):  # iterate over two rats
-                x = pos.data[r, 0, bodypart, :]
-                y = pos.data[r, 1, bodypart, :]
-                
-                # Remove NaNs
-                mask = ~np.isnan(x) & ~np.isnan(y)
-                x, y = x[mask], y[mask]
-                if len(x) == 0:
-                    continue
+            
+            #Get both rats x and y data
+            x0 = pos.data[0, 0, bodypart, :]
+            x1 = pos.data[1, 0, bodypart, :]
+            y0 = pos.data[0, 1, bodypart, :]
+            y1 = pos.data[1, 1, bodypart, :]
+            
+            '''x0 = [100, 200, 100, 400, 300, 400, 500, 600, 100, 400, 350, 550]
+            y0 = [350, 400, 300, 100, 500, 550, 400, 500, 300, 400, 300, 300]
+            
+            x1 = [1300, 1200, 1250, 1100, 1000, 800, 400, 700, 900, 900, 1000, 1100]
+            y1 = [100, 100, 30, 20, 50, 200, 175, 150, 125, 80, 100, 50]'''
+            
+            if (len(x0) != len(x1) or len(x0) != len(y0) or len(x0) != len(y1)):
+                print("MISMATCH IN LENGTHS")
+            
+            #Remove NaNs
+            '''mask = ~np.isnan(x0) & ~np.isnan(y0)
+            x0, y0 = x0[mask], y0[mask]
+            mask = ~np.isnan(x1) & ~np.isnan(y1)
+            x1, y1 = x1[mask], y1[mask]'''
+            
+            averageX0 = np.mean(x0)
+            averageX1 = np.mean(x1)
+            
+            print("averageX0: ", averageX0)
+            print("averageX1: ", averageX1)
+            
+            if (averageX0 > averageX1):
+                left_x.append(x1)
+                left_y.append(y1)
+                right_x.append(x0)
+                right_y.append(y0)
+            else:
+                left_x.append(x0)
+                left_y.append(y0)
+                right_x.append(x1)
+                right_y.append(y1)
 
-                # Classify left vs right preference
-                if np.mean(x < midline) > 0.5:
-                    left_x.append(x)
-                    left_y.append(y)
-                else:
-                    right_x.append(x)
-                    right_y.append(y)
 
         left_x = np.concatenate(left_x) if left_x else np.array([])
         left_y = np.concatenate(left_y) if left_y else np.array([])
@@ -137,7 +161,49 @@ class dataAnalysis:
 
         return left_x, left_y, right_x, right_y
         
-    
+    def generateUpDownHeatmapData(self, bodypart = 3):
+        """
+        Generate concatenated left vs right position data across experiments.
+        Returns: up_x, up_y, down_x, down_y
+        """
+        up_x, up_y = [], []
+        down_x, down_y = [], []
+
+        for i, exp in enumerate(self.experiments):
+            pos = exp.pos
+            
+            #Get both rats x and y data
+            x0 = pos.data[0, 0, bodypart, :]
+            x1 = pos.data[1, 0, bodypart, :]
+            y0 = pos.data[0, 1, bodypart, :]
+            y1 = pos.data[1, 1, bodypart, :]
+            
+            if (len(x0) != len(x1) or len(x0) != len(y0) or len(x0) != len(y1)):
+                print("MISMATCH IN LENGTHS")
+            
+            averageY0 = np.mean(y0)
+            averageY1 = np.mean(y1)
+            
+            print("averageY0: ", averageY0)
+            print("averageY1: ", averageY1)
+            
+            if (averageY0 > averageY1):
+                up_x.append(x1)
+                up_y.append(y1)
+                down_x.append(x0)
+                down_y.append(y0)
+            else:
+                up_x.append(x0)
+                up_y.append(y0)
+                down_x.append(x1)
+                down_y.append(y1)
+
+        up_x = np.concatenate(up_x) if up_x else np.array([])
+        up_y = np.concatenate(up_y) if up_y else np.array([])
+        down_x = np.concatenate(down_x) if down_x else np.array([])
+        down_y = np.concatenate(down_y) if down_y else np.array([])
+
+        return up_x, up_y, down_x, down_y
     
 class createGraphs:
     def __init__(self, arena_width=1392, arena_height=640):
@@ -159,12 +225,13 @@ class createGraphs:
         df.to_csv(savepath, index=False)
         print(f"Saved positions to {savepath}")
 
-    def makeHeatmap(self, x, y, bins=100, sigma=2, clip_percent=97, gamma=0.2):
+    def makeHeatmap(self, x, y, bins=150, sigma=2, clip_percent=97, gamma=0.3):
         if len(x) == 0:
             return None
         xedges = np.linspace(0, self.arena_width, bins)
         yedges = np.linspace(0, self.arena_height, bins)
         H, _, _ = np.histogram2d(x, y, bins=[xedges, yedges])
+        H = H.T #transpose so x→cols, y→rows
         H = gaussian_filter(H, sigma=sigma)
         vmax = np.percentile(H, clip_percent)
         H = np.clip(H, 0, vmax)
@@ -180,9 +247,9 @@ class createGraphs:
         fig_height = fig_width / self.aspect_ratio
         plt.figure(figsize=(fig_width, fig_height))
         im = plt.imshow(
-            np.flipud(H),
+            H,
             extent=[0, self.arena_width, 0, self.arena_height],
-            origin="lower",
+            origin="upper",
             aspect="auto",
             cmap="hot"
         )
@@ -190,6 +257,8 @@ class createGraphs:
         plt.xlabel("X position (px)")
         plt.ylabel("Y position (px)")
         plt.title(title)
+        # Invert Y axis so 0 is top, max at bottom
+        plt.gca().invert_yaxis()
         plt.savefig(filename, dpi=300, bbox_inches="tight")
         plt.show()
         plt.close()
@@ -203,6 +272,7 @@ class createGraphs:
 #
 
 #Real
+
 filtered = "/gpfs/radev/project/saxena/drb83/rat-cooperation/David/Behavioral_Quantification/Sorted_Data_Files/Filtered.csv"
 def getFiltered():
     fe = fileExtractor(filtered)
@@ -232,7 +302,6 @@ sessions = arr[7]
 ratPairs = arr[8]        
 
 
-
 #Test
 '''
 lev_files = ["/Users/david/Documents/Research/Saxena_Lab/rat-cooperation/David/Behavioral_Quantification/Example_Data_Files/041824_Cam3_TrNum5_Coop_KL007Y-KL007G_lever.csv", "/Users/david/Documents/Research/Saxena_Lab/rat-cooperation/David/Behavioral_Quantification/Example_Data_Files/041824_Cam3_TrNum11_Coop_KL007Y-KL007G_lever.csv"]
@@ -241,16 +310,47 @@ mag_files = ["/Users/david/Documents/Research/Saxena_Lab/rat-cooperation/David/B
 
 pos_files = ["/Users/david/Documents/Research/Saxena_Lab/rat-cooperation/David/Behavioral_Quantification/Example_Data_Files/041824_Cam3_TrNum5_Coop_KL007Y-KL007G.predictions.h5", "/Users/david/Documents/Research/Saxena_Lab/rat-cooperation/David/Behavioral_Quantification/Example_Data_Files/041824_Cam3_TrNum11_Coop_KL007Y-KL007G.predictions.h5"]
 
+h5_file1 = "/Users/david/Documents/Research/Saxena_Lab/rat-cooperation/David/Behavioral_Quantification/Example_Data_Files/041624_Cam3_TrNum7_Coop_KL002B-KL002Y.predictions.h5"
+h5_file2 = "/Users/david/Documents/Research/Saxena_Lab/rat-cooperation/David/Behavioral_Quantification/Example_Data_Files/041624_Cam3_TrNum9_Coop_KL002B-KL002Y.predictions.h5"
+h5_file = "/Users/david/Documents/Research/Saxena_Lab/rat-cooperation/David/Behavioral_Quantification/Example_Data_Files/4_nanerror_test.h5"
+pos_files = [h5_file]
+
 fpsList = [30, 30]
 totFramesList = [15000, 15000]
 initialNanList = [0.15, 0.12]
-dates = ['04172006', '8172006'] # 
-sessions = ['id1', 'id2'] #
-ratPairs = [] #
-'''     
+dates = [datetime(2024, 4, 16, 0, 0), datetime(2024, 4, 18, 0, 0)] # 
+sessions = ['000000_TrNum1', '000000_TrNum2'] #
+ratPairs = ['KL001Y-KL001G', 'KL007Y-KL007G'] #
 
+'''
+
+#Generate Graphs
+#
+#
 
 data = dataAnalysis(mag_files, lev_files, pos_files, fpsList, totFramesList, initialNanList, dates, sessions, ratPairs, prefix = "", save=True)
+
+# Generate left/right position data
+left_x, left_y, right_x, right_y = data.generateLeftRightHeatmapData()
+up_x, up_y, down_x, down_y = data.generateUpDownHeatmapData()
+
+# Create the graph object
+graphs = createGraphs()
+
+# Save CSV
+#graphs.savePositionsCSV(left_x, left_y, right_x, right_y, savepath="left_vs_right_positions.csv")
+
+# Make heatmaps
+H_left = graphs.makeHeatmap(left_x, left_y)
+H_right = graphs.makeHeatmap(right_x, right_y)
+H_up = graphs.makeHeatmap(up_x, up_y)
+H_down = graphs.makeHeatmap(down_x, down_y)
+
+# Save heatmaps
+graphs.saveHeatmap(H_left, "Left-preferring Group Heatmap", "left_heatmap.png")
+graphs.saveHeatmap(H_right, "Right-preferring Group Heatmap", "right_heatmap.png")
+graphs.saveHeatmap(H_up, "Up-preferring Group Heatmap", "up_heatmap.png")
+graphs.saveHeatmap(H_down, "Down-preferring Group Heatmap", "down_heatmap.png")
 
         
         
