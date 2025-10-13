ff68b412a5c5383e0bce860241f210cdba955277
DavidBackerPeral <david.backerperal@yale.edu>
2025-10-13 09:35:55 -0400

    test

--- David/Behavioral_Quantification/Scripts/graph_creator.py
+++ David/Behavioral_Quantification/Scripts/graph_creator.py
@@ -2480,8 +2480,8 @@ def getGroupRatPairs():
 
 data = getGroupRatPairs()
 pairGraphs = MicePairGraphs(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9])
-#pairGraphs.lineGraphStrategies()
-pairGraphs.lineGraphGazing()
+pairGraphs.lineGraphStrategies()
+#pairGraphs.lineGraphGazing()
 
 #pairGraphs.lineGraphLeverBias()
 
