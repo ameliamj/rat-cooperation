d9a93ce4b32667e45df3d929020a5980a9744b62
DavidBackerPeral <david.backerperal@yale.edu>
2025-10-18 17:53:53 -0400

    test

--- David/Behavioral_Quantification/Scripts/allDataCSVsCreator.py
+++ David/Behavioral_Quantification/Scripts/allDataCSVsCreator.py
@@ -1020,5 +1020,5 @@ metadata_smol = "/gpfs/radev/project/saxena/drb83/rat-cooperation/David/Behavior
 metadata_filtered = "/gpfs/radev/project/saxena/drb83/rat-cooperation/David/Behavioral_Quantification/Sorted_Data_Files/Filtered.csv"
 
 creator = allDataCSVsCreator(metadata_smol, post="")
-creator.createTrialCSVStandard()
-#creator.createEventCSVStandard()
+#creator.createTrialCSVStandard()
+creator.createEventCSVStandard()
