cfe99a7f0e3e5d1f167590bd62f8bc8a9fa8617c
DavidBackerPeral <david.backerperal@yale.edu>
2025-12-21 18:23:05 -0500

    test

--- David/Behavioral_Quantification/Scripts/file_extractor_class.py
+++ David/Behavioral_Quantification/Scripts/file_extractor_class.py
@@ -1004,8 +1004,8 @@ def saveAllCSVs():
 #saveAllCSVs()
 
 
-fe = fileExtractor(fixedExpanded)
-fe.deleteOnlyFullyInvalid(saveFile=True)
+#fe = fileExtractor(fixedExpanded)
+#fe.deleteOnlyFullyInvalid(saveFile=True)
 
 
 
