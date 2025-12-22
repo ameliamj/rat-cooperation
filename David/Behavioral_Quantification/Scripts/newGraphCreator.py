b7389214fe691e4f4038cc9c14335705b0485e01
DavidBackerPeral <david.backerperal@yale.edu>
2025-12-22 05:47:05 -0500

    test

--- David/Behavioral_Quantification/Scripts/newGraphCreator.py
+++ David/Behavioral_Quantification/Scripts/newGraphCreator.py
@@ -1160,7 +1160,7 @@ def getFiltered():
     return [fe.getLevsDatapath(), fe.getMagsDatapath(), fe.getPosDatapath(), fpsList, totFramesList, initial_nan_list, dates, sessions, ratPairs, familiarity, transparency]
 
 def minRequirements():
-    fe = fileExtractor(minReqTraining)
+    fe = fileExtractor(minReqTesting)
     fe.data = fe.deleteBadNaN()
     fe.deleteOnlyFullyInvalid()
     #fe.filterOutBadNums()
@@ -1313,7 +1313,7 @@ graphs = createGraphs()
 #
 # Gaze Graphs: Coop vs NonCoop
 #
-suffix = "training"
+suffix = "testing"
 
 gazePerSessionCoop, isKL = data.gazingData(
     csv_path= f"coop_gazing_{suffix}.csv"
